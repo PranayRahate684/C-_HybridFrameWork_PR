bd2cba7cd2bacd0647e35d5cfece138830bf40d4
EAD\6124529 <pranayrahate16@gmail.com>
2024-09-24 11:47:22 +0530

    Add project files.

---
+++ OpenEMR-MedicalRecordAutomation/Login.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenEMR_MedicalRecordAutomation
+{
+    public class Login
+    {
+        public void logintest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("http://demo.openemr.io/b/openemr/interface/login/login.php?");
+
+          string actualText=  driver.Title;
+            Assert.That(actualText, Is.EqualTo("OpenEMR Login"));
+                
+        }
+        [Test]
+        public void ApplicationDescriptionTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+
+            //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
+            string descrption = driver.FindElement(By.CssSelector("[class='text-center lead']")).Text;
+            string actualDescrption = "The most popular open-source Electronic Health Record and Medical Practice Management solution.";
+
+            Assert.That(descrption, Is.EqualTo(actualDescrption));
+            driver.Quit();
+        
+        }
+        
+    }
+}
